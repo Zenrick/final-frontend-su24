9a1ea761e83441d562c72c8e85151898313544da
Zenrick <zenricka@gmail.com>
2024-08-17 02:10:18 -0400

    feature added: select new task priority level

--- src/components/views/NewTaskView.jsx
+++ src/components/views/NewTaskView.jsx
@@ -1,12 +1,31 @@
+import { Link } from "react-router-dom";
+
 function NewTaskView({ handleSubmit }) {
+    let priortityMarkup = (
+      <p>Priority level:
+        <label>
+          <input type="radio" name="taskPriority" value="1" /> Low
+        </label>
+        <label>
+          <input type="radio" name="taskPriority" value="2" /> Medium
+        </label>
+        <label>
+          <input type="radio" name="taskPriority" value="3" /> High
+        </label>
+      </p>
+    );
     return (
-        <section>
+        <section >
           <h2>Add a New Task</h2>
-          <form onSubmit={handleSubmit}>
-            <label htmlFor="taskContent">Task content:</label>
-            <input type="text" id="taskContent" defaultValue="" required />
+          <form onSubmit={handleSubmit} id="newtaskform">
+            <label>
+              Text input: <input name="taskContent" />
+            </label>  
+            {priortityMarkup}
             <button>Save Task</button>
           </form>
+          <br/>
+          <Link to={`/tasks`}><button style={{margin: "8px"}}>Back to All Tasks</button></Link>
         </section>
       )
 
