4ab33909fc1d185ac2544ff77b5f7e5bb8f79389
Zenrick <zenricka@gmail.com>
2024-08-17 01:48:16 -0400

    updating task components

--- src/components/views/SingleTaskView.jsx
+++ src/components/views/SingleTaskView.jsx
@@ -1,20 +1,20 @@
 function SingleTaskView({task}) {
-    if (!task) {
-        return (
-          <section>
-            <h2>Task not found!</h2>
-          </section>
-        );
-      }
-    
+  if (!task) {
       return (
         <section>
-          <article className="task">
-            <h2>{task.title}</h2>
-            <p className="task-title">{task.title}</p>
-          </article>
+          <h2>Task not found!</h2>
         </section>
       );
+    }
+  
+    return (
+      <section>
+        <article className="task">
+          <h2>{task.content}</h2>
+          <p className="task-title">{task.content}</p>
+        </article>
+      </section>
+    );
 }
 
 export default SingleTaskView;
