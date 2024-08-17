d72ac9b0a2973ade342d355676d9c7df8a0212de
Zenrick <zenricka@gmail.com>
2024-08-17 01:50:18 -0400

    implemented delete task action

--- src/store/tasksSlice.js
+++ src/store/tasksSlice.js
@@ -5,6 +5,8 @@ export function tasksReducer(state = initialState, action) {
     switch (action.type) {
       case 'tasks/tasksLoaded':
         return action.payload;
+      case 'tasks/taskDeleted':
+        return state.filter(task => task.id!==action.payload);
       default:
         return state;
     }
@@ -13,13 +15,26 @@ export function tasksReducer(state = initialState, action) {
 //API calls go here
 import axios from "axios";
 //PATH (should be where your server is running)
-const PATH = "http://localhost:5001/api";
+const PATH = "http://localhost:5001/api/tasks";
 
-//Thunk 
+//Thunks
+
+/* GET ALL TASKS */
 export const fetchTasks = () => async (dispatch) => {
   try {
-    let res = await axios.get(`${PATH}/tasks`);
+    let res = await axios.get(`${PATH}`);
     dispatch({type: 'tasks/tasksLoaded', payload: res.data});
+  } catch(err) {
+    console.error(err);
+  }
+};
+
+/* DELETE TASK */
+export const deleteTask = taskId => async dispatch => {
+  try {
+    await axios.delete(`${PATH}/${taskId}`);
+    //delete succesful so change state with dispatch
+    dispatch({type: 'tasks/taskDeleted', payload: taskId});
   } catch(err) {
     console.error(err);
   }
